3dc026884e6836f86daabbd5e6f646c13ef44a24
米津舞優 <89147867+MayuYonezu@users.noreply.github.com>
2022-09-27 04:05:54 +0900

    ファイルを追加しました

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-$(function($){
-    $('.slider').slick();
+jQuery(function($) {
+    $(document).ready(function(){
+        $('.slider').slick({
+            dots:false,
+            slidesToShow:3,
+            responsive:[
+                {
+                    breakpoint: 1024,
+                    settings:{
+                        slidesToShow:3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings:{
+                        slidesToShow:2,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings:{
+                        slidesToShow:1,
+                    }
+                },
+            ]
+        });
+    });
 });
